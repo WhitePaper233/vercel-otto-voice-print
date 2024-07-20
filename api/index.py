de74715a8ca699fe26431dfc192555463bcc6a66
WhitePaper233 <1423634265@qq.com>
2024-07-20 16:39:16 +0800

    feat: Add support for generating audio files with customizable options

--- api/index.py
+++ api/index.py
@@ -1,3 +1,4 @@
+import uuid
 from flask import Flask, request, send_file
 
 from api.otto import OTTO
@@ -9,11 +10,19 @@ app = Flask(__name__)
 def api():
     # 读取参数
     content = request.args.get('content', '')
+    raw_mode = request.args.get('raw_mode', True)
+    pitch = request.args.get('pitch', 1.0)
+    speed = request.args.get('speed', 1.0)
+    norm = request.args.get('norm', True)
     if not content:
-        return 
+        return
     
+    # 生成音频
     otto = OTTO()
-    otto.generate(content)
-    otto.export_file('/tmp/output.wav')
+    otto.generate(content, raw_mode, pitch, speed, norm)
 
-    return send_file('/tmp/output.wav', as_attachment=True)
+    # 导出文件
+    file_name = uuid.uuid4().hex
+    otto.export_file(f'/tmp/{file_name}.wav')
+
+    return send_file(f'/tmp/{file_name}.wav', as_attachment=True)
